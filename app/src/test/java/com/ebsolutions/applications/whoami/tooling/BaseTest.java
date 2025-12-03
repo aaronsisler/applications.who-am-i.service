7b5ae0a408d9a7fb7f256aedfff82597303cb11b
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2025-12-03 18:55:50 -0500

    Lots of fun learnings to reduce the noise coming from running tests. Less logging of info is better

--- app/src/test/java/com/ebsolutions/applications/whoami/tooling/BaseTest.java
+++ app/src/test/java/com/ebsolutions/applications/whoami/tooling/BaseTest.java
@@ -1,9 +1,11 @@
 package com.ebsolutions.applications.whoami.tooling;
 
+import com.ebsolutions.applications.whoami.appuser.core.AppUserRepository;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import io.cucumber.spring.CucumberContextConfiguration;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
@@ -11,6 +13,7 @@ import org.springframework.test.web.servlet.MockMvc;
 @SpringBootTest
 @AutoConfigureMockMvc
 @CucumberContextConfiguration
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 public class BaseTest {
   protected final ObjectMapper objectMapper = new ObjectMapper()
       .findAndRegisterModules()
@@ -19,4 +22,7 @@ public class BaseTest {
 
   @Autowired
   protected MockMvc mockMvc;
+
+  @Autowired
+  private AppUserRepository appUserRepository;
 }
