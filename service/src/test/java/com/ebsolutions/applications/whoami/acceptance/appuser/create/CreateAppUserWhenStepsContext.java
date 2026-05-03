173679812f55dba3c864f74f3a28524c10878e6c
Aaron Sisler <aaron.sisler@eandbsolutions.com>
2026-05-03 13:10:56 -0400

    Cleaning up the app.yamls per phase/env

--- service/src/test/java/com/ebsolutions/applications/whoami/acceptance/appuser/create/CreateAppUserWhenStepsContext.java
+++ service/src/test/java/com/ebsolutions/applications/whoami/acceptance/appuser/create/CreateAppUserWhenStepsContext.java
@@ -11,22 +11,18 @@ public class CreateAppUserWhenStepsContext extends AcceptanceStepsContext {
   @When("the client submits the create-user request")
   public void theClientSubmitsTheCreateUserRequest() {
 
-    try {
-      this.acceptanceScenarioContext.response = restClient
-          .post()
-          .uri(ApiPaths.APP_USERS_PATH)
-          .contentType(this.acceptanceScenarioContext.requestContentType)
-          .body(this.acceptanceScenarioContext.requestPayload)
-          .exchange((request, response) -> {
-            String body = new String(response.getBody().readAllBytes(), StandardCharsets.UTF_8);
+    this.acceptanceScenarioContext.response = restClient
+        .post()
+        .uri(ApiPaths.APP_USERS_PATH)
+        .contentType(this.acceptanceScenarioContext.requestContentType)
+        .body(this.acceptanceScenarioContext.requestPayload)
+        .exchange((request, response) -> {
+          String body = new String(response.getBody().readAllBytes(), StandardCharsets.UTF_8);
 
-            return ResponseEntity
-                .status(response.getStatusCode())
-                .headers(response.getHeaders())
-                .body(body);
-          });
-    } catch (Exception e) {
-      System.out.println("Error during request submission: " + e.getMessage());
-    }
+          return ResponseEntity
+              .status(response.getStatusCode())
+              .headers(response.getHeaders())
+              .body(body);
+        });
   }
 }
